bb7afce9f61df70c319d6fb42296c8d94055ea22
Garland Lai and Marina Poltorak <garland@>
2019-06-03 14:55:44 -0700

    edits the BMI calculater to parse float

--- js/scripts.js
+++ js/scripts.js
@@ -25,17 +25,18 @@ alert("The product of " + number1 + " * " + number2 + " = " + result + ".");
 var divide = function(number1,number2) {
 	return number1 / number2;
 }
-//var number1 = parseInt(prompt("Enter a number: "));
-//var number2 = parseInt(prompt("Enter another number: "));
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
 var result = divide(number1,number2);
 alert("The quotient of " + number1 + " / " + number2 + " = " + result + ".");
+
 // BMI
 var bodyMassIndex = function(weight, height) {
 	return weight / (height * height) * 703;
 };
 
-var weight = parseInt(prompt("Your weight in lbs: "));
-var height = parseInt(prompt("Your height in inches: "));
+var weight = parseFloat(prompt("Your weight in lbs: "));
+var height = parseFloat(prompt("Your height in inches: "));
 var result = bodyMassIndex(weight, height).toFixed(2);
 
 alert("Your BMI is " + result);
