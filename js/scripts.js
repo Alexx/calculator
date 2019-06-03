ba051b0a3a2ca61942bddf229d88b69f74a0fd2b
Garland Lai and Marina Poltorak <garland@>
2019-06-03 10:12:18 -0700

    add functionality to -,*,/

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 var add = function(number1,number2) {
 	return number1 + number2;
 }
-
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
+var result = add(number1,number2);
+alert("The sum of " + number1 + " + " + number2 + " = " + result + ".");
 
-alert(add(number1,number2));
+var subtract = function(number1,number2) {
+	return number1 - number2;
+}
+//var number1 = parseInt(prompt("Enter a number: "));
+//var number2 = parseInt(prompt("Enter another number: "));
+var result = subtract(number1,number2);
+alert("The difference of " + number1 + " - " + number2 + " = " + result + ".");
+
+var multiply = function(number1,number2) {
+	return number1 * number2;
+}
+//var number1 = parseInt(prompt("Enter a number: "));
+//var number2 = parseInt(prompt("Enter another number: "));
+var result = multiply(number1,number2);
+alert("The product of " + number1 + " * " + number2 + " = " + result + ".");
+
+var divide = function(number1,number2) {
+	return number1 / number2;
+}
+//var number1 = parseInt(prompt("Enter a number: "));
+//var number2 = parseInt(prompt("Enter another number: "));
+var result = divide(number1,number2);
+alert("The quotient of " + number1 + " / " + number2 + " = " + result + ".");
