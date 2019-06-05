e0b70ce301b5feda4c4713a8e63b797b430e75d6
Alex Siegberg and Garland Lai <alex@>
2019-06-05 11:48:05 -0700

    add user interface to calculator

---
+++ js/old-code.js
@@ -0,0 +1,66 @@
+// var add = function(number1,number2) {
+// 	return number1 + number2;
+// }
+// var number1 = parseInt(prompt("Enter a number: "));
+// var number2 = parseInt(prompt("Enter another number: "));
+// var result = add(number1,number2);
+// alert("The sum of " + number1 + " + " + number2 + " = " + result + ".");
+//
+// var subtract = function(number1,number2) {
+// 	return number1 - number2;
+// }
+// //var number1 = parseInt(prompt("Enter a number: "));
+// //var number2 = parseInt(prompt("Enter another number: "));
+// var result = subtract(number1,number2);
+// alert("The difference of " + number1 + " - " + number2 + " = " + result + ".");
+//
+// var multiply = function(number1,number2) {
+// 	return number1 * number2;
+// }
+// //var number1 = parseInt(prompt("Enter a number: "));
+// //var number2 = parseInt(prompt("Enter another number: "));
+// var result = multiply(number1,number2);
+// alert("The product of " + number1 + " * " + number2 + " = " + result + ".");
+//
+// var divide = function(number1,number2) {
+// 	return number1 / number2;
+// }
+// var number1 = parseInt(prompt("Enter a number: "));
+// var number2 = parseInt(prompt("Enter another number: "));
+// var result = divide(number1,number2);
+// alert("The quotient of " + number1 + " / " + number2 + " = " + result + ".");
+//
+// // BMI
+// var bodyMassIndex = function(weight, height) {
+// 	return weight / (height * height) * 703;
+// };
+//
+// var weight = parseFloat(prompt("Your weight in lbs: "));
+// var height = parseFloat(prompt("Your height in inches: "));
+// var result = bodyMassIndex(weight, height).toFixed(2);
+//
+// alert("Your BMI is " + result);
+//
+// // Temp Conversion
+//
+// var convToFahrenheit = function(degreeCelcius) {
+// 	return degreeCelcius * 1.8 + 32;
+// };
+//
+// var degreeCelcius = parseInt(prompt("Temperature in Celcius: "));
+//
+// var result = convToFahrenheit(degreeCelcius).toFixed(2);
+//
+// alert("The temperature in Fahrenheit " + result);
+//
+//
+//
+// var convToCelcius = function(degreeFahrenheit) {
+// 	return (degreeFahrenheit - 32) / 1.8;
+// };
+//
+// var degreeFahrenheit = parseInt(prompt("Temperature in Fahrenheit: "));
+//
+// var result2 = convToCelcius(degreeFahrenheit).toFixed(2);
+//
+// alert("The temperature in Celcius " + result2);
--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,51 @@
-var add = function(number1,number2) {
-	return number1 + number2;
-}
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-var result = add(number1,number2);
-alert("The sum of " + number1 + " + " + number2 + " = " + result + ".");
+//Business logic
 
-var subtract = function(number1,number2) {
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
 	return number1 - number2;
-}
-//var number1 = parseInt(prompt("Enter a number: "));
-//var number2 = parseInt(prompt("Enter another number: "));
-var result = subtract(number1,number2);
-alert("The difference of " + number1 + " - " + number2 + " = " + result + ".");
+};
 
-var multiply = function(number1,number2) {
+var multiply = function(number1, number2) {
 	return number1 * number2;
-}
-//var number1 = parseInt(prompt("Enter a number: "));
-//var number2 = parseInt(prompt("Enter another number: "));
-var result = multiply(number1,number2);
-alert("The product of " + number1 + " * " + number2 + " = " + result + ".");
+};
 
-var divide = function(number1,number2) {
+var divide = function(number1, number2) {
 	return number1 / number2;
-}
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-var result = divide(number1,number2);
-alert("The quotient of " + number1 + " / " + number2 + " = " + result + ".");
-
-// BMI
-var bodyMassIndex = function(weight, height) {
-	return weight / (height * height) * 703;
 };
 
-var weight = parseFloat(prompt("Your weight in lbs: "));
-var height = parseFloat(prompt("Your height in inches: "));
-var result = bodyMassIndex(weight, height).toFixed(2);
+//User interface logic
 
-alert("Your BMI is " + result);
+$(function() {
 
-// Temp Conversion
-
-var convToFahrenheit = function(degreeCelcius) {
-	return degreeCelcius * 1.8 + 32;
-};
-
-var degreeCelcius = parseInt(prompt("Temperature in Celcius: "));
-
-var result = convToFahrenheit(degreeCelcius).toFixed(2);
-
-alert("The temperature in Fahrenheit " + result);
-
-
-
-var convToCelcius = function(degreeFahrenheit) {
-	return (degreeFahrenheit - 32) / 1.8;
-};
-
-var degreeFahrenheit = parseInt(prompt("Temperature in Fahrenheit: "));
-
-var result2 = convToCelcius(degreeFahrenheit).toFixed(2);
-
-alert("The temperature in Celcius " + result2);
+	$("#add").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var result = (add(number1, number2));
+		$("#output").text(result);
+	});
+	$("#subtract").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var result = (subtract(number1, number2));
+		$("#output").text(result);
+	});
+	$("#multiply").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var result = (multiply(number1, number2));
+		$("#output").text(result);
+	});
+	$("#divide").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var result = (divide(number1, number2));
+		$("#output").text(result);
+	});
+});
